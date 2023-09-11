a330a36abd36d14dde49c92df34f65a40e81f3bc
jbadillo <jbadillo@illumina.com>
2023-09-11 15:22:26 -0700

    upload files

---
+++ frontend/src/app/ethos/ethos.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { EthosComponent } from './ethos.component';
+
+
+
+@NgModule({
+  declarations: [
+    EthosComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class EthosModule { }
