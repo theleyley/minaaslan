61fffbff13071e0404ed39bf66d1e6dbf434a50e
jbadillo <jbadillo@illumina.com>
2023-09-13 21:41:52 -0700

    add form

--- frontend/src/app/contact/contact.component.ts
+++ frontend/src/app/contact/contact.component.ts
@@ -2,6 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import {FormGroup, FormControl, Validators} from '@angular/forms';
 import {Subscription} from 'rxjs';
 import {Router} from '@angular/router';
+import {map} from 'rxjs/operators';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {environment} from '../../environments/environment';
 
 @Component({
   selector: 'app-contact',
@@ -12,15 +16,19 @@ export class ContactComponent implements OnInit {
   payload = {};
   formSubscription: Subscription = new Subscription();
   onSubmission = false;
+  baseUrl: string = '';
+
   contactForm = new FormGroup({
     fullName: new FormControl('', [Validators.required]),
     email: new FormControl('', [Validators.required]),
     subject: new FormControl('', [Validators.required]),
     message: new FormControl<string>('', [Validators.required])
   });
-  constructor(private router: Router) { }
+  constructor(private router: Router, private http: HttpClient) {
+  }
 
   ngOnInit(): void {
+    this.baseUrl = `${environment.apiBase}`;
     this.formSubscription = this.contactForm.valueChanges.subscribe((values: any) => {
       this.constructPayload(values);
     });
@@ -28,17 +36,30 @@ export class ContactComponent implements OnInit {
 
   constructPayload(values: any) {
     this.payload = {
-      fullname: values.fullName,
-      email: values.email,
-      subject: values.subject,
-      message: values.message
+      token: 'recaptcha token',
+      formName: 'Mina Aslan Contact Form',
+      formData: {
+        fullname: values.fullName,
+        email: values.email,
+        subject: values.subject,
+        message: values.message
+      }
     }
   }
 
   onSubmit() {
-    console.log(this.payload);
-    this.onSubmission = true;
-    }
+    const token = 'recaptcha token';
+    const url = `${this.baseUrl}/api/ezforms/submit`;
+    this.http.post<any>(url, {token, formData: this.payload}).pipe(map((response) => {
+              // Success
+              this.onSubmission = true;
+            }, (err: any) => {
+              // Error
+              this.onSubmission = true;
+
+            })
+        ).subscribe();
+  }
 
   route(route: any) {
     this.router.navigate([`${route}`]);
