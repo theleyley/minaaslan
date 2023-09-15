89d8cdfdc1ede6e8084665fe995555899fce29c0
Leyla Badillo <lbadillo@illumina.com>
2023-09-15 01:16:57 -0600

    updating database to match digitalocean cluster db config

--- backend/config/env/production/database.js
+++ backend/config/env/production/database.js
@@ -1,17 +1,23 @@
+
+const parse = require("pg-connection-string").parse;
+
+const { host, port, database, user, password } = parse(
+  process.env.DATABASE_URL
+);
+
 module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('DATABASE_HOST', '127.0.0.1'),
-      port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'strapi'),
-      user: env('DATABASE_USERNAME', 'strapi'),
-      password: env('DATABASE_PASSWORD', 'strapi'),
-      schema: env('DATABASE_SCHEMA', 'public'), // Not required
+      host,
+      port,
+      database,
+      user,
+      password,
       ssl: {
-        ca: env('DATABASE_CA'),
+        rejectUnauthorized: false,
       },
     },
-    debug: false,
+      debug: false,
   },
 });
